74718969a7138f28a29a3f292f08b7784d4a732e
Salvador Bravo <sa.bravo0901@gmail.com>
2026-05-09 13:43:13 -0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className=" text-red-500">Hola, Mundo!</Text>
+    </View>
+  );
+}
